e51f1f7b99fe8b86790cd5245f065d8d827a609e
Admin <admin@MBP-de-apple.lan>
2024-07-01 09:53:14 +0200

    init config

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module golang-hexagon
 
 go 1.22.3
+
+require github.com/jinzhu/configor v1.2.2
+
+require (
+	github.com/BurntSushi/toml v1.2.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ internal/app/infrastructure/config/config.go
@@ -0,0 +1,34 @@
+package config
+
+import "github.com/jinzhu/configor"
+
+type Kafka struct {
+	Broker  string
+	Topic   string
+	GroupId string
+}
+
+type Mongo struct {
+	ConnectionString string
+	Collection       string
+}
+
+type AppConfig struct {
+	Environment string
+	Port        string
+	Kafka       string
+	Mongo       string
+}
+
+func NewAppConfig(path string) (*AppConfig, error) {
+	config := new(AppConfig)
+	// configor is a package that allow to automatically inject config
+	// into a struct with a path
+	err := configor.
+		New(&configor.Config{ErrorOnUnmatchedKeys: true}).
+		Load(config, path)
+	if err != nil {
+		return nil, err
+	}
+	return config, nil
+}
